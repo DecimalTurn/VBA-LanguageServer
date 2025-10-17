141acf1a31ce32542e82dd28f7a1777ded79ee53
Martin Leduc <31558169+DecimalTurn@users.noreply.github.com>
2025-10-16 22:35:21 -0400

    Fix postCreateCommand

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
+set -euo pipefail
+
+# Run from repository root when executed as a postCreateCommand
+cd "$(pwd)"
+
 npm ci
-npm run textMate
+npm i -g @vscode/vsce
+npm run build:textMate
 npm run antlr
 npm run mvantlr
 
