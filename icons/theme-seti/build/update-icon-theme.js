a9fe7d19f1ea2ab15eae552fcc587a4dec251425
Martin Leduc <31558169+DecimalTurn@users.noreply.github.com>
2026-02-26 22:03:17 -0500

    Update inheritIconFromLanguage section based on https://github.com/microsoft/vscode/pull/297749/changes

--- icons/theme-seti/build/update-icon-theme.js
+++ icons/theme-seti/build/update-icon-theme.js
@@ -49,7 +49,11 @@ const inheritIconFromLanguage = {
 	"jsonl": 'json',
 	"postcss": 'css',
 	"django-html": 'html',
-	"blade": 'php'
+	"blade": 'php',
+	"prompt": 'markdown',
+	"instructions": 'markdown',
+	"chatagent": 'markdown',
+	"skill": 'markdown'
 };
 
 const ignoreExtAssociation = {
